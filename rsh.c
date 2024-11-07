b136538221912588d2e2c277769bc273c7466958
Tyler Collingridge <tylercollingridge@gmail.com>
2024-11-07 14:12:24 -0700

    feat: create spawn process function
    - Null terminate tokLine arr

--- rsh.c
+++ rsh.c
@@ -29,10 +29,46 @@ int isAllowed(const char *cmd)
 	return 0;
 }
 
+int spawn_process(char *argv[])
+{
+	pid_t pid;
+	int status;
+	posix_spawnattr_t attr;
+
+	// Initialize spawn attributes
+	posix_spawnattr_init(&attr);
+
+	// Set flags if needed, for example, to specify the scheduling policy
+	// posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSCHEDULER);
+
+	// Spawn a new process
+	if (posix_spawnp(&pid, argv[0], NULL, &attr, argv, environ) != 0)
+	{
+		perror("spawn failed");
+		exit(EXIT_FAILURE);
+	}
+
+	// Wait for the spawned process to terminate
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		perror("waitpid failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (WIFEXITED(status))
+	{
+		printf("Spawned process exited with status %d\n", WEXITSTATUS(status));
+	}
+
+	// Destroy spawn attributes
+	posix_spawnattr_destroy(&attr);
+
+	return EXIT_SUCCESS;
+}
+
 void cd(const char *TARGET)
 {
-	// FIX: check if target dir exists (not sure what dir structure looks like - can't implement this check yet)
-	// FIX: do I need a statement if cd fails?
+	// FIX: check if target dir exists?
 	if (chdir(TARGET) != 0)
 	{
 		printf("-rsh: cd: fail\n");
@@ -46,8 +82,8 @@ int main()
 
 	char line[256];
 	char lineCopy[256];
-	const int NUM_ARGS = 20; // only supposed to have 20 arguments max
-	char *tokLine[NUM_ARGS]; // tokenized line
+	const int NUM_ARGS = 20;		 // only supposed to have 20 arguments max
+	char *tokLine[NUM_ARGS + 1]; // tokenized line
 	int tokenCount = 0;
 
 	while (1)
@@ -76,14 +112,13 @@ int main()
 			token = strtok(NULL, " ");
 		}
 
+		tokLine[tokenCount] = NULL;
+
 		// check if cmd is valid - check first element in line arr
 		if (isAllowed(tokLine[0]) == 0)
 		{
 			printf("NOT ALLOWED!\n");
 		}
-
-		// implement cmds 1-9:
-		// create process...
 
 		// 10. cd:
 		if (strcmp(tokLine[0], "cd") == 0)
@@ -93,22 +128,26 @@ int main()
 			{
 				printf("-rsh: cd: too many arguments\n");
 			}
-			// FIX: do I need a check for too few args?
+
+			// FIX: statement for too few args?
 
 			// else call cd using the single arg as the target dir
 			cd(tokLine[1]);
 		}
-
-		// 11. exit:
-		if (strcmp(tokLine[0], "exit") == 0)
+		else if (strcmp(tokLine[0], "exit") == 0) // 11. exit:
 		{
 			return 0;
 		}
-
-		// 12. help:
-		if (strcmp(tokLine[0], "help") == 0)
+		else if (strcmp(tokLine[0], "help") == 0) // 12. help:
 		{
 			printf("The allowed commands are:\n1: cp\n2: touch\n3: mkdir\n4: ls\n5: pwd\n6: cat\n7: grep\n8: chmod\n9: diff\n10: cd\n11: exit\n12: help\n");
+		}
+		else // cmds 1-9:
+		{
+			if (spawn_process(tokLine) != 0)
+			{
+				printf("-rsh: spawn process failed\n");
+			}
 		}
 	}
 	return 0;
